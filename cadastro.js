4c77aef5381bd4d762b0a31bb26667ecd6f5ec96
devabinadabe <abinadabedev@outlook.com>
2024-05-20 22:10:09 -0300

    Adicionando função de comentar mensagem

--- cadastro.js
+++ cadastro.js
@@ -3,3 +3,10 @@ function signUp(email){
 }
 
 /// pegando o email e cadastrando na plataforma
+
+function mostramensagem(){
+    return 'Bem vindo ao sistema';
+}
+
+
+/// pegando o email e cadastrando na plataforma
